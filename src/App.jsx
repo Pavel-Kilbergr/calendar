2b47e74f43443d9c5ccc7bbf191e8ba071fb25d5
Pavel Kilbergr <pavel.kilbergr@gmail.com>
2024-11-10 20:53:11 +0100

    fin css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CalendarApp from './Components/CalendarApp.jsx';
+import './Components/CalendarApp.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <CalendarApp />
+    </div>
+  );
+};
+
+export default App;
