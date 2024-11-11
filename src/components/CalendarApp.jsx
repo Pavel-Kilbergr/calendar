d1898150c761e979be33b836ce9c5339b653ceb9
Pavel Kilbergr <pavel.kilbergr@gmail.com>
2024-11-11 22:19:50 +0100

    Add today

--- src/components/CalendarApp.jsx
+++ src/components/CalendarApp.jsx
@@ -46,10 +46,18 @@ export const CalendarApp = () => {
     const clickedDate = new Date(currentYear, currentMonth, day);
     const today = new Date();
 
-    if (clickedDate >= today) {
+    if (clickedDate >= today || isSameDay(clickedDate, today)) {
       setSelectedDate(clickedDate);
       setShowEventPopup(true);
     }
+  };
+
+  const isSameDay = (date1, date2) => {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
   };
 
   return (
